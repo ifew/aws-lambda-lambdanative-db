d21412225de037b455a222590ef0890d2141b31a
iFew <chitpong@gmail.com>
2019-05-03 17:13:00 +0700

    delete profile

--- src/aws-lambda-lambdanative/build_linux.sh
+++ src/aws-lambda-lambdanative/build_linux.sh
@@ -10,4 +10,4 @@ rm -f $(pwd)/package.zip
 docker run --rm -v $(pwd):/app lambdanative:3.0
 cp bin/Release/netcoreapp3.0/linux-x64/native/aws-lambda-lambdanative bootstrap
 zip package.zip bootstrap
-aws s3 cp package.zip s3://backend-layer --profile sansiri
+#aws s3 cp package.zip s3://backend-layer
