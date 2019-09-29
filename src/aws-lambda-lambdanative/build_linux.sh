759d6d360037c2154f2f4f87da2e79851a3ae627
iFew <chitpong@gmail.com>
2019-09-30 01:40:05 +0700

    Updated to .Net Core 3.0.100

--- src/aws-lambda-lambdanative/build_linux.sh
+++ src/aws-lambda-lambdanative/build_linux.sh
@@ -7,7 +7,8 @@
 
 rm -f $(pwd)/bootstrap
 rm -f $(pwd)/package.zip
-docker run --rm -v $(pwd):/app lambdanative:3.0
+docker run --rm -v $(pwd):/app lambdareadytorun:3.0
 cp bin/Release/netcoreapp3.0/linux-x64/native/aws-lambda-lambdanative bootstrap
+#cp bin/Release/netcoreapp3.0/linux-x64/publish/aws-lambda-lambdanative bootstrap
 zip package.zip bootstrap
 #aws s3 cp package.zip s3://backend-layer
